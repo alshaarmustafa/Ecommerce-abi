63854133e63f2ce3aed58d9a08b473c7cbcdf6a4
Mustafa Alshaar <mustafa5555885555@gmail.com>
2026-04-08 10:35:19 +0300

    Refactor DB connection logic and improve logging for MongoDB connection

--- config/db.js
+++ config/db.js
@@ -1,13 +1,23 @@
-//DB connection 
+// DB connection 
 const mongoose = require("mongoose");
-require('dotenv').config()
-const url =process.env.MONGODB_URI
+require('dotenv').config();
+
+const url = process.env.MONGODB_URI;
+
 mongoose.connect(url)
-  .then(() => {
-    console.log(
-      "✅ MongoDB Local Connected to DB:",
-      mongoose.connection.db.databaseName
-    );
+  .then((conn) => {
+    //conn.connection.host بيعطيك عنوان السيرفر الحقيقي
+    const host = conn.connection.host;
+    const dbName = conn.connection.name;
+
+    console.log(`✅ MongoDB Connected to DB: ${dbName}`);
+    
+    // فحص بسيط عشان تعرف أنت وين متصل فعلياً
+    if (host.includes('mongodb.net')) {
+      console.log(`🌐 Remote Host: Atlas (Cloud)`);
+    } else {
+      console.log(`💻 Remote Host: Localhost`);
+    }
   })
   .catch((err) => console.error("❌ MongoDB connection error:", err));
 
